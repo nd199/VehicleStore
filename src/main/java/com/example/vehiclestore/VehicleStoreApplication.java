dc7b823440cba0fb65d33518be173de8261fa2ac
nd199 <narend251999@gmail.com>
2022-07-01 20:11:53 +0530

    First initalizing project commit

---
+++ src/main/java/com/example/vehiclestore/VehicleStoreApplication.java
@@ -0,0 +1,13 @@
+package com.example.vehiclestore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehicleStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VehicleStoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/vehiclestore/VehicleStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vehiclestore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehicleStoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
